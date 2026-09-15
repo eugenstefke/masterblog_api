433b6782b35207eaba4328be151f6647be5529c1
Eugen Stefke <eugen.stefke@web.de>
2026-09-15 13:14:23 +0200

    POSTS moved to posts.json

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,13 +1,39 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+import json
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+def get_json():
+    """
+    Read the json file
+    return: all blogs as a dict in list [{},{}] structure
+    """
+
+    with open("posts.json", "r", encoding="utf-8") as data:
+        return json.load(data)
+
+def write_json(post):
+    """
+    write the python blog posts to json structure
+    """
+
+    with open("posts.json", "w", encoding="utf-8") as data:
+        json.dump(post, data, indent=4, ensure_ascii=False)
 
 def validate_post_data_content_and_title(data):
     if not data.get("content") and not data.get("title"):
@@ -32,10 +58,11 @@ def validate_post_data_title(data):
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
 
+    all_blogs = get_json()
     sort_query = request.args.get('sort')
     direction_query = request.args.get('direction')
 
-    results = list(POSTS)
+    results = list(all_blogs)
 
     if sort_query:
         if sort_query.lower() not in ("title", "content"):
@@ -52,11 +79,12 @@ def get_posts():
 def add_posts():
 
     data = request.get_json()
+    all_blogs = get_json()
 
-    if len(POSTS) == 0:
+    if len(all_blogs) == 0:
         new_id = 1
     else:
-        new_id = max(blog_post['id'] for blog_post in POSTS) + 1
+        new_id = max(blog_post['id'] for blog_post in all_blogs) + 1
 
     new_blog_post = {
         "id": new_id,
@@ -73,34 +101,37 @@ def add_posts():
     if not validate_post_data_title(new_blog_post):
         return jsonify({"error": "missing title data"}), 400
 
-    POSTS.append(new_blog_post)
+    all_blogs.append(new_blog_post)
+    write_json(all_blogs)
     return jsonify(new_blog_post), 201
 
 
 @app.route('/api/posts/<int:postID>', methods=['DELETE'])
 def delete_post(postID):
+    all_blogs = get_json()
 
-    global POSTS
-
-    post_exists = any(blog["id"] == postID for blog in POSTS)
+    post_exists = any(blog["id"] == postID for blog in all_blogs)
 
     if not post_exists:
         return jsonify({"message": f"Post with id {postID} not found."}), 404
 
-    POSTS = [blog for blog in POSTS if blog["id"] != postID]
+    all_blogs = [blog for blog in all_blogs if blog["id"] != postID]
+    write_json(all_blogs)
     return jsonify({"message": f"Post with id {postID} has been deleted successfully."}), 200
 
 
 @app.route('/api/posts/<int:postID>', methods=['PUT'])
 def update(postID):
-    
-    for blog in POSTS:
+
+    all_blogs = get_json()
+    for blog in all_blogs:
         if blog["id"] == postID:
-            data = request.get_json()
+            data = request.get_json() # liest den Body der eingehenden HTTP-Anfrage (also die Daten, die der Client beim PUT-Request mitgeschickt hat), interpretiert ihn als JSON-Text und wandelt ihn in ein Python-Dict um.
 
             blog["title"] = data.get('title', blog['title'])
             blog["content"] = data.get('content', blog['content'])
 
+            write_json(all_blogs)
             return jsonify(blog), 200
 
     return jsonify({"message": f"Post with id {postID} not found."}), 404
@@ -111,8 +142,10 @@ def search():
     title_query = request.args.get('title', '')
     content_query = request.args.get('content', '')
 
+    all_blogs = get_json()
+
     results = []
-    for blog in POSTS:
+    for blog in all_blogs:
         title_match = title_query.lower() in blog['title'].lower() if title_query else False
         content_match = content_query.lower() in blog['content'].lower() if content_query else False
 
@@ -120,9 +153,6 @@ def search():
             results.append(blog)
 
     return jsonify(results)
-
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
